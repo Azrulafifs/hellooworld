4eb008c1ddc83469faf789c2a5efa97eb5e110cf
ASUS <ASUS@LAPTOP-JO0O7ME7>
2024-03-04 14:52:38 +0700

    hello world

---
+++ src/ahul/Ahul.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ahul;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Ahul {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("hello world!");
+    System.out.println("y");
+    }
+}
